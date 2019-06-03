9fbdc9c08811954ac3d8cba81a0ce496a894384f
vitor-amartins <vmam@cin.ufpe.br>
2019-06-03 20:00:04 -0300

    Changed app name

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ require('./app/routes/404-route.js')(app);
 const port = 3000;
 
 app.listen(process.event.PORT || port, () => {
+// app.listen(port, () => {
     console.log(`Listening on port ${port} (or not)`);
 });
