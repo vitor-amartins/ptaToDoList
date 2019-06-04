5cf9a85089c989e3a8ce903de85fb8de70aa0465
vitor-amartins <31016577+vitor-amartins@users.noreply.github.com>
2019-06-04 09:02:25 -0300

    Fixed app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require('./config/server.js');
 require('./app/routes/home-route.js')(app);
 require('./app/routes/404-route.js')(app);
 
-app.listen(process.event.PORT || 3000, () => {
+app.listen(process.eve.PORT || 3000, () => {
 // app.listen(3000, () => {
     console.log(`Listening on port 3000 (or not)`);
 });
